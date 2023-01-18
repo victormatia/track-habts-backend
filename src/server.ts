333fc121a21706e1e1cc746ce692c420bccc2b5f
Victor Matias <fvictor1705@gmail.com>
2023-01-18 11:44:34 -0300

    feat: install prisma

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,10 @@
 import fastify from "fastify";
 
 const app = fastify();
+const PORT: number = 3333;
 
 app.get('/', () => 'Hello World');
 
-app.listen({ port: 3333 });
+app.listen({ port: PORT })
+  .then(() => console.log(`Server running on port ${PORT}!`));
 
