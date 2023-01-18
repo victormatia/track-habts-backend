e55c9cd17b109533f789b3d42b1b3aa0469cf761
Victor Matias <fvictor1705@gmail.com>
2023-01-18 12:14:47 -0300

    refactor: update url from '/' 'to Hello'

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import fastify from "fastify";
 const app = fastify();
 const PORT: number = 3333;
 
-app.get('/', () => 'Hello World');
+app.get('/Hello', () => 'Hello World');
 
 app.listen({ port: PORT })
   .then(() => console.log(`Server running on port ${PORT}!`));
