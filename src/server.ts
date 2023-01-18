5517dff543a315d32c9cf13fb77e5fe29e84a5c7
Victor Matias <fvictor1705@gmail.com>
2023-01-18 12:36:16 -0300

    feat: connect the backend with the database

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,20 @@
 import fastify from "fastify";
+import { PrismaClient } from "@prisma/client";
 
 const app = fastify();
+
+// Aqui instanciamos o prisma para ter acesso ao banco de dados
+const prisma = new PrismaClient();
+
 const PORT: number = 3333;
 
 app.get('/Hello', () => 'Hello World');
+
+app.get('/habits', async () => {
+  const habits = await prisma.habit.findMany();
+
+  return habits;
+});
 
 app.listen({ port: PORT })
   .then(() => console.log(`Server running on port ${PORT}!`));
